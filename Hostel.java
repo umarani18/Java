f23d52cb1eb8a72677d198bb6301543a9bcf19ac
umarani18 <umarani180305@gmail.com>
2025-10-01 17:48:12 +0530

    adding programs

--- Hostel.java
+++ Hostel.java
@@ -1,40 +1,40 @@
 public class Hostel {
- int i;
- static int s = 10;
- 
- {
-  System.out.println("Testing block");
- }
- 
- public static void print() {
-  System.out.println(s + " static print " + k);
- }
- 
- static {
-  System.out.println("Testing static block : " + s + " " + k);
- }
- 
- Hostel(){
-  System.out.println(i + " " + j);
- }
- 
- public static void main(String arg[]) {
-  Hostel obj = new Hostel();
-  obj.i = 100;
-  obj.j = 200;
-  obj.display();
-  
-  Hostel obj2 = new Hostel();
-  
- }
- public void display() {
-  System.out.println(i + " - " + j);
- }
- 
- static int k = 5;
- int j;
- 
- static {
-  System.out.println(s + "- static -" + k);
- }
+    int i;
+    static int s = 10;
+    
+    {
+        System.out.println("Testing block");
+    }
+    
+    public static void print() {
+        //System.out.println(s + " static print " + k); //original
+        System.out.println(s + " static print " + Hostel.k); //ans`wer
+    }
+    
+    static {
+        System.out.println("Testing static block : " + s + " " + k);
+    }
+    
+    Hostel(){
+        System.out.println(i + " " + j);
+    }
+    
+    public static void main(String arg[]) {
+        Hostel obj = new Hostel();
+        obj.i = 100;
+        obj.j = 200;
+        obj.display();
+        
+        Hostel obj2 = new Hostel();
+    }
+    public void display() {
+        System.out.println(i + " - " + j);
+    }
+    
+    static int k = 5;
+    int j;
+    
+    static {
+        System.out.println(s + "- static -" + k);
+    }
 }
